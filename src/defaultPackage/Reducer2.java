e26bb4a433a7b68efdcadd503e735e5277d7435e
varshneyanmol <varshneyanmol@yahoo.com>
2017-01-19 21:23:31 +0530

    removed zero and  NaN values from similarity score

--- src/defaultPackage/Reducer2.java
+++ src/defaultPackage/Reducer2.java
@@ -6,51 +6,54 @@ import java.util.Iterator;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class Reducer2 extends Reducer<Text, Text, Text, Text>{
-	
+public class Reducer2 extends Reducer<Text, Text, Text, Text> {
+
 	@Override
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 		double similarityScore = 0.0;
 		String[] X_Y;
 		double n = 0.0;
-		
+
 		double sigmaX = 0.0;
 		double sigmaY = 0.0;
 		double sigmaXY = 0.0;
 		double sigmaXsqr = 0.0;
 		double sigmaYsqr = 0.0;
-		
+
 		double X = 0.0;
 		double Y = 0.0;
-		
+
 		Iterator<Text> itr = values.iterator();
-		while(itr.hasNext()) {
+		while (itr.hasNext()) {
 			n += 1.0;
 			X_Y = itr.next().toString().split(",");
-			
+
 			X = Double.parseDouble(X_Y[0]);
 			Y = Double.parseDouble(X_Y[1]);
 
 			sigmaX += X;
 			sigmaY += Y;
-			sigmaXY += X*Y;
-			sigmaXsqr += X*X;
-			sigmaYsqr += Y*Y;
+			sigmaXY += X * Y;
+			sigmaXsqr += X * X;
+			sigmaYsqr += Y * Y;
 		}
-		
+
 		if (n == 1) {
-			similarityScore = 0.0;
-			
+			similarityScore = 0.0; // no need to write beacuse only one user has
+									// rated both the movies.
+
 		} else {
-			
+
 			double numerator = (n * sigmaXY) - (sigmaX * sigmaY);
-			double denominator = Math.sqrt((n * sigmaXsqr - (sigmaX*sigmaX)) * (n * sigmaYsqr - (sigmaY*sigmaY)));
+			double denominator = Math.sqrt((n * sigmaXsqr - (sigmaX * sigmaX)) * (n * sigmaYsqr - (sigmaY * sigmaY)));
 			similarityScore = numerator / denominator;
+			if (!Double.isNaN(similarityScore)) {
+				context.write(key, new Text(String.valueOf(similarityScore)));
+			}
 		}
-		
+
 		System.out.println("in reducer 2");
 		System.out.println(key.toString() + " : " + similarityScore + " n = " + n);
-		context.write(key, new Text(String.valueOf(similarityScore)));
 
 	}
 }
